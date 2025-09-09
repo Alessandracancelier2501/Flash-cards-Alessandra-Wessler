a9a85d8d960133e81529df3a54fd0c403e4e24da
Alessandracancelier2501 <alessandra.cancelier@escola.pr.gov.br>
2025-09-09 08:45:14 -0300

    09/09

---
+++ Perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'geografia',
+    'Qual é o maior país do mundo em termos de área?',
+    'Rússia'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o elemento químico com o símbolo "S"?',
+    'Enxofre'
+)
+
+criaCartao(
+    'Gerais',
+    'Qual é o nome da rede social criada por Mark Zuckerberg?',
+    'Facebook'
+)
+
+criaCartao(
+    'Gerais',
+    'Quem pintou a famosa obra "A Noite Estrelada"?',
+    'Vincent van Gogh.'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde se localiza Machu Picchu?',
+    'Peru'
+)
+
+criaCartao(
+    'Gerais',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison.'
+)
+
+criaCartao(
+    'Gerais',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison.'
+)
