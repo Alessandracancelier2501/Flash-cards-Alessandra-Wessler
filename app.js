a70c37a5442c9f52c4263b9865f80d2d33fd7e8e
Alessandracancelier2501 <alessandra.cancelier@escola.pr.gov.br>
2025-09-16 10:47:34 -0300

    16/09

--- app.js
+++ app.js
@@ -14,8 +14,15 @@ function criaCartao(categoria, pergunta, resposta) {
 </div>
 </div>`
 
+let respostaEstaVisivel= false
+
+function viraCartao(){
+    respostaEstaVisivel=!respostaEstaVisivel
+}
+
 
     container.appendChild(cartao)
+
 
 
 }
